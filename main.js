8d2854194e1672d11da66053126d480c530fd7ba
miltonpulley <notifymilton@gmail.com>
2025-01-31 17:57:25 +1100

    Made the project list generate from a JSON file. Made project list show error card if projects couldn't be loaded. Added date, blurb and folder path to be stored for each project. Tweaked style.css.

--- main.js
+++ main.js
@@ -11,11 +11,12 @@ const ProjectCategoriesAndTags =
 
 /// Session State Storage
 /// =====================
+const AllProjects = {};
 const ActiveFilters =
 {
 	whitelisted: [],
 	blacklisted: [],
-}
+};
 
 /// Initialisation Functions
 /// ========================
@@ -30,14 +31,43 @@ window.onload = () => // event handler
 	let tagList = document.getElementsByClassName("tagfilterbutton");
 	for(let t of tagList)
 	{
-		t.onclick = () => SetTagFilter(t.value)
+		t.onclick = () => SetTagFilter(t.value);
 	};
 
 	// Generate the list of projects
-	//const ProjectList = document.getElementById("filterprojectcategoriestags");
-	//ProjectList.innerHTML = ConstructProjectList();
+	FetchAllProjects();
 }
 
+function FetchAllProjects()
+{
+	FetchJSON("/projectlist.json", FetchAllProjectsCallback);
+}
+function FetchAllProjectsCallback(fetchedprojectsJSON)
+{
+	// clear the projects array
+	while(AllProjects.length > 0)
+	{
+		AllProjects.pop();
+	}
+
+	fetchedprojectsJSON.projects.forEach((proj) =>
+	{
+		if(AllProjects[proj.name])
+		{
+			console.error(`Error: Project of name \"${proj.name}\" already exists!`);
+		}
+		else
+		{
+			AllProjects[proj.name] = new Project(proj.name, new Date(proj.date), proj.folderpath, proj.tags, proj.blurb);
+		}
+	});
+	//console.log(AllProjects)
+
+	ConstructProjectList();
+}
+
+/// Page and HTML construction
+/// ==========================
 /*\
 |*| <ul id="filterprojectcategories">
 |*| |   ...
@@ -71,15 +101,45 @@ const ConstructProjectCategoryList = () =>
 	return result;
 }
 
+/*\
+|*| <ul id="projectlist">
+|*| |   ...
+|*| |   <li class="project">
+|*| |   |   <button value="[Project Name]" class="projectexpand">Expand</button>
+|*| |   |   <img src="[Project Image]" class="projectimg"></img>
+|*| |   |   <p class="projectdate">[Date]</p>
+|*| |   |   <p class="projectname">[Project Name]</p>
+|*| |   |   <p class="projectdesc">[Project Description]</p>
+|*| |   |   <ul class="projecttaglist">
+|*| |   |   |   ...
+|*| |   |   |   <li class="projecttag">[Project Tag]</li>
+|*| |   |   |   ...
+|*| |   |   </ul>
+|*| |   </li>
+|*| |   ...
+|*| </ul>
+\*/
 const ConstructProjectList = () =>
 {
-	let result = "<ul>";
-	
-
-
-	return result + "</ul>";
+	let result = "";
+	for(const [name, project] of Object.entries(AllProjects))
+	{
+		result += `<li class=\"project\">`;
+		result += `<button class="projectexpand" value=\"${project.name}\">Expand</button>`;
+		result += `<img class=\"projectimg\" src=\"${project.folderpath}/thumbnail.png\"></img>`;
+		result += `<p class=\"projectdate\">${project.date.toLocaleDateString()}</p>`;
+		result += `<p class=\"projectname\">${project.name}</p>`;
+		result += `<p class=\"projectdesc\">${project.blurb}</p>`;
+		result += `<ul class=\"projecttaglist\">`;
+		project.tags.forEach((t) =>
+		{
+			result += `<li class=\"projecttag\">${t}</li>`;
+		});
+		result += "</ul></li>";
+	};
+	const ProjectList = document.getElementById("projectlist");
+	//ProjectList.innerHTML = result;
 }
-
 
 /// Runtime Functions
 /// =================
@@ -88,24 +148,49 @@ function SetTagFilter(tag)
 	console.log(tag);
 }
 
+function FetchJSON(directory, callbackFunction)
+{
+	// fetch is asynchronous, and returns a "promise", that sometime in the future, there will be some data.
+	// as it takes time to fetch from the server .then() is used to handle it.
+	fetch(directory)
+	.then((response) =>
+	{
+		//console.log(response); // logs the PROMISE
+		return response.json(); // tells the code to parse the response as a JSON payload
+	})
+	.catch((error) =>
+	{
+		console.error(`Error in fetching JSON \"${directory}\", ${error}`);
+	})
+	.then((FetchedParsedJSONdata) => // THIS is the data that was promised
+	{
+		//console.log(FetchedParsedJSONdata); // logs the data
+		callbackFunction(FetchedParsedJSONdata);
+	});
+}
+
 /// Classes and Objects
 /// ===================
 class Project
 {
 	name = "";
-	year = new Date();
+	date = new Date();
+	folderpath = "";
 	tags = [];
+	blurb = "";
 
-	constructor(name, year, tags)
+	constructor(name, date, folderpath, tags, blurb)
 	{
 		this.name = name;
-		this.year = year;
+		this.date = date;
+		this.folderpath = folderpath;
 		this.tags = tags;
+		this.blurb = blurb
 	}
 
 	describe()
 	{
-		return this.name + " (" + this.year.toString() + "), " + this.tags;
+		return this.name + " (" + this.date.toString() + "), " + this.folderpath + "," + this.tags + ", " + this.blurb;
 	}
 }
-const proj1_portfolio = new Project("Portfolio Page", new Date(2025, 1, 30, 12, 41, 26), ["Solo", "HTML/CSS/JS", "2D digital"]);
+// const proj1_portfolio = new Project("Portfolio Page", new Date(2025, 1, 30, 12, 41, 26), "", ["Solo", "HTML/CSS/JS", "2D digital"]);
